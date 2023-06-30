18c4ef8f951822476adfe3ed4a4ad6ed6482fdad
Anastasiya M <mega.gara11@gmail.com>
2023-06-30 15:01:21 +0700

    add src/

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 300;
+        int depositAmount = 1200;
+
+        int bonus;
+        if (depositAmount > 1000) {
+            System.out.println((bonus = depositAmount / 100) + " бонусных рублей");
+        } else {
+            System.out.println((bonus = 0) + " бонусных рублей");
+        }
+        int finalAccount = depositAmount + initialAccount;
+        System.out.println((finalAccount) + " рублей итоговый счет");
+    }
 }
